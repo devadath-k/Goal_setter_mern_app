2f247f69489f0c53a85750c5541a9edc5af278a8
devadath-k <devadathdathu7@gmail.com>
2024-08-04 10:29:56 +0530

    rest api for goals

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log("Hello, World!"); 
+const express = require('express');
+const color = require('colors')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+const {errorHandler} = require('./middleware/errorMiddleware')
+
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, ()=> console.log(`Server running on port ${port}`));
