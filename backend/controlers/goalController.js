907d7a640078a633a5c4270cf4acdd05f4b89ec7
devadath-k <devadathdathu7@gmail.com>
2024-08-06 03:22:22 +0530

    Almost completed

--- backend/controlers/goalController.js
+++ backend/controlers/goalController.js
@@ -38,14 +38,13 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
 
-    if(!user){
+    if(!req.bodyuser){
         res.status(401)
         throw new error('User not found')
     }
 
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized')
     }
@@ -66,14 +65,12 @@ const deleteGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
-
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new error('User not found')
     }
 
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized')
     }
---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+
+function Spinner() {
+  return (
+    <div className= 'loadingSpinnercontainer'>
+        <div className="loadingSpinner"></div>
+    </div>
+  )
+}
+
+export default Spinner
